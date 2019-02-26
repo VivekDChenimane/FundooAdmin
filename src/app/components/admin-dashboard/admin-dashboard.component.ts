0b538ae5666a5faec5361c44e167ade91ee1e2e7
Vivek <vivekdchenimane@gmail.com>
2019-02-26 11:45:21 +0530

    search complete

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -7,13 +7,50 @@ declare var $: any;@Component({
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
-  basic = 0; advance = 0;preBasic = 0;  char = '';
-  array = []; mainArray = []; 
+  basic = 0; advance = 0;preBasic = 0; preAdvance = 0; char = '';
+  array = []; mainArray = []; pre = '';
   constructor(private httpService: HttpService, private router: Router) { }
 
   ngOnInit() {
     this.getUserList();
+  // $(document).ready(function(){
+  //   $("button").click(function(){
+  //     $(".advance").fadeToggle("slow");
+  //     $(".basic").fadeToggle("slow");
+  //   });
+  // });
   }
+  values: any = '';
+  //when user hit in search button it works
+onKeyUp(event: any) { 
+  //storing value in previous
+  this.pre = this.values;
+  this.values = event.target.value;
+  if (this.values.length < this.pre.length) {//when user hit back space
+    this.array = this.mainArray;
+  }
+  //method for name filtering
+  var checkName = (item: any) => {
+    // console.log('item ',item.firstName.toLowerCase(),'  value ',this.values.toLowerCase())
+    return item.firstName.toLowerCase().startsWith(this.values.toLowerCase());
+  }
+  //array filter
+  this.array = this.array.filter(function (item) {
+    // console.log('item',item);
+    return checkName(item);
+  });
+  for (var i = 0; i < this.array.length; i++) {
+    this.char = this.array[i].service;
+    if (this.char == 'basic' || this.char == 'Basic' || this.char == 'BASIC') {
+      this.preBasic++;
+    } else {
+      this.preAdvance++;
+    }
+  }
+  this.basic = this.preBasic, this.advance = this.preAdvance;
+  this.preBasic = 0, this.preAdvance = 0;
+
+};
   getUserList() {
     try {
     this.httpService.getRequest('user/getAdminUserList').subscribe(data => {
